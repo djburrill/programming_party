e62b892db1e2b916c9343fed8b7bda09fbbe4584
djburrill <danieljburrill@gmail.com>
2016-01-07 16:57:44 -0500

    - Finished project 1

--- djb/proj1/python/molGeom.py
+++ djb/proj1/python/molGeom.py
@@ -9,19 +9,47 @@
 
 # Imports
 import numpy as np
+import periodic as per
+import scipy.constants as sc
 
 # Classes
 class atom():
     ## Constructor
     def __init__(self,atomicNum,xPos,yPos,zPos):
+        # Atomic number
         self.atomicNumber = atomicNum
+
+        # Position
         self.xPos = xPos
         self.yPos = yPos
         self.zPos = zPos
 
+        # Average mass
+        self.mass = per.element(self.atomicNumber).mass
+
+    def translate(self,dx,dy,dz):
+        '''
+            Translate the position of the atom to [x+dx,y+dy,z+dz].
+
+            INPUT
+                dx: (float) Translation in x
+                dy: (float) Translation in y
+                dz: (float) Translation in z
+
+            OUTPUT
+                No output. Updates atom position.
+        '''
+
+        self.xPos += dx
+        self.yPos += dy
+        self.zPos += dz
+
 class molecule():
     ## Constructor
     def __init__(self,inFileName):
+        # Set tolerance
+        self.tol = 0.000001
+
         # Initialize atom list
         self.atomList = []
 
@@ -32,16 +60,43 @@ class molecule():
         for index,aN in enumerate(self.atomicNum):
             self.atomList.append(atom(aN,self.xPos[index],self.yPos[index],self.zPos[index]))
 
-    def bondLength(self,atom1,atom2):
+    def unitVec(self,atom1,atom2):
         '''
-            Calculate bond length a1-a2
+            Calculate the unit vector from atom1 to atom2.
 
             INPUT
                 atom1: (atom class)
                 atom2: (atom class)
 
             OUTPUT
-                Returns (float) in distance units
+                Returns list (float).
+        '''
+
+        # Define atom positions
+        r1 = [atom1.xPos,atom1.yPos,atom1.zPos]
+        r2 = [atom2.xPos,atom2.yPos,atom2.zPos]
+
+        # Calculate bond lengths
+        r12 = self.bondLength(atom1,atom2)
+
+        # Catch the case where two atoms are the same
+        if (r12 <= self.tol):
+            return [0.0,0.0,0.0]
+
+        # Calculate unit vectors
+        return [(r2[0]-r1[0])/r12,(r2[1]-r1[1])/r12,(r2[2]-r1[2])/r12]
+
+
+    def bondLength(self,atom1,atom2):
+        '''
+            Calculate bond length a1-a2.
+
+            INPUT
+                atom1: (atom class)
+                atom2: (atom class)
+
+            OUTPUT
+                Returns (float) in distance units.
         '''
 
         # Define positions
@@ -52,7 +107,7 @@ class molecule():
 
     def bondAngle(self,atom1,atom2,atom3):
         '''
-            Calculate bond angle a1-a2-a3
+            Calculate bond angle a1-a2-a3.
 
             INPUT
                 atom1: (atom class)
@@ -60,31 +115,27 @@ class molecule():
                 atom3: (atom class)
 
             OUTPUT
-                Returns (float) in radians
+                Returns (float) in radians.
         '''
-
-        # Define positions
-        r1 = [atom1.xPos,atom1.yPos,atom1.zPos]
-        r2 = [atom2.xPos,atom2.yPos,atom2.zPos]
-        r3 = [atom3.xPos,atom3.yPos,atom3.zPos]
-
-        # Calculate bond lengths
-        r12 = self.bondLength(atom1,atom2)
-        r23 = self.bondLength(atom2,atom3)
-
-        # Catch the case where two atoms are the same
-        if ((r12 <= 0.00000001) or (r23 <= 0.00000001)):
-            return 0.0
 
         # Calculate unit vectors
-        e1 = [(r2[0]-r1[0])/r12,(r2[1]-r1[1])/r12,(r2[2]-r1[2])/r12]
-        e2 = [(r3[0]-r2[0])/r23,(r3[1]-r2[1])/r23,(r3[2]-r2[2])/r23]
+        e1 = self.unitVec(atom2,atom1)
+        e2 = self.unitVec(atom2,atom3)
 
-        return np.arccos(np.dot(e1,e2))
+        # Calculate argument of arccos()
+        sol = np.dot(e1,e2)
 
-    def OOPAngle(atom1,atom2,atom3,atom4):
+        # Check that returned value does not produce infinities
+        if (sol < -1):
+            return np.arccos(-1)
+        elif (sol > 1):
+            return np.arccos(1)
+        else:
+            return np.arccos(sol)
+
+    def OOPAngle(self,atom1,atom2,atom3,atom4):
         '''
-            Calculate out-of-plane angle a1->a3-a2-a4
+            Calculate out-of-plane angle a1->a3-a2-a4.
 
             INPUT
                 atom1: (atom class) Out-of-plane atom
@@ -93,77 +144,143 @@ class molecule():
                 atom4: (atom class)
 
             OUTPUT
-                Returns (float) in radians
+                Returns (float) in radians.
         '''
 
-        # Define positions
-        r1 = [atom1.xPos,atom1.yPos,atom1.zPos]
-        r2 = [atom2.xPos,atom2.yPos,atom2.zPos]
-        r3 = [atom3.xPos,atom3.yPos,atom3.zPos]
-        r4 = [atom4.xPos,atom4.yPos,atom4.zPos]
-
-        # Calculate bond lengths
-        r13 = self.bondLength(atom1,atom3)
-        r23 = self.bondLength(atom2,atom3)
-        r34 = self.bondLength(atom4,atom3)
-
-        # Catch the case where two atoms are the same
-        if ((r23 <= 0.00000001) or (r34 <= 0.00000001) or (r13 <= 0.00000001)):
-            return 0.0
-
         # Calculate unit vectors
-        e23 = [(r3[0]-r2[0])/r23,(r3[1]-r2[1])/r23,(r3[2]-r2[2])/r23]
-        e34 = [(r4[0]-r3[0])/r34,(r4[1]-r3[1])/r34,(r4[2]-r3[2])/r34]
-        e13 = [(r3[0]-r1[0])/r13,(r3[1]-r1[1])/r13,(r3[2]-r1[2])/r13]
+        e32 = self.unitVec(atom3,atom2)
+        e34 = self.unitVec(atom3,atom4)
+        e31 = self.unitVec(atom3,atom1)
 
-        crossProd = np.cross(e23,e34)
+        crossProd = np.cross(e32,e34)
         leftVec = crossProd/np.sin(self.bondAngle(atom2,atom3,atom4))
 
-        return np.arcsin(np.dot(leftVec,e13))
+        # Calculate argument of arcsin()
+        sol = np.dot(leftVec,e31)
 
-    def torsionAngle(atom1,atom2,atom3,atom4):
+        # Check that arcsin does not produce infinities
+        if (sol < -1):
+            return np.arcsin(-1)
+        elif (sol > 1):
+            return np.arcsin(1)
+        else:
+            return np.arcsin(sol)
+
+    def torsionAngle(self,atom1,atom2,atom3,atom4):
         '''
-            Calculate torsion/dihedral angle a1->a3-a2-a4
+            Calculate torsion/dihedral angle a1-a2-a3-a4.
 
             INPUT
-                atom1: (atom class) Out-of-plane atom
+                atom1: (atom class)
                 atom2: (atom class)
-                atom3: (atom class) Central atom
+                atom3: (atom class)
                 atom4: (atom class)
 
             OUTPUT
-                Returns (float) in radians
+                Returns (float) in radians.
         '''
 
-        # Define positions
-        r1 = [atom1.xPos,atom1.yPos,atom1.zPos]
-        r2 = [atom2.xPos,atom2.yPos,atom2.zPos]
-        r3 = [atom3.xPos,atom3.yPos,atom3.zPos]
-        r4 = [atom4.xPos,atom4.yPos,atom4.zPos]
-
-        # Calculate bond lengths
-        r12 = bondLength(atom1,atom2)
-        r23 = bondLength(atom2,atom3)
-        r34 = bondLength(atom4,atom3)
-
-        # Catch the case where two atoms are the same
-        if ((r23 <= 0.00000001) or (r34 <= 0.00000001) or (r13 <= 0.00000001) or (r12 <= 0.00000001)):
-            return 0.0
-
         # Calculate unit vectors
-        e23 = [(r3[0]-r2[0])/r23,(r3[1]-r2[1])/r23,(r3[2]-r2[2])/r23]
-        e34 = [(r4[0]-r3[0])/r34,(r4[1]-r3[1])/r34,(r4[2]-r3[2])/r34]
-        e12 = [(r2[0]-r1[0])/r12,(r2[1]-r1[1])/r12,(r2[2]-r1[2])/r12]
+        e23 = self.unitVec(atom2,atom3)
+        e34 = self.unitVec(atom3,atom4)
+        e12 = self.unitVec(atom1,atom2)
 
         # Calculate cross products
         crossProd1 = np.cross(e12,e23)
         crossProd2 = np.cross(e23,e34)
 
         # Calculate bond angles
-        bondAngle1 = bondAngle(atom1,atom2,atom3)
-        bondAngle2 = bondAngle(atom2,atom3,atom4)
+        bondAngle1 = self.bondAngle(atom1,atom2,atom3)
+        bondAngle2 = self.bondAngle(atom2,atom3,atom4)
 
-        return np.arccos(np.dot(crossProd1,crossProd2)/(bondAngle1*bondAngle2))
+        # Compute argument of arccos (Check for infinity)
+        if ((bondAngle1 != 0) and (bondAngle2 != 0.0)):
+            sol = np.dot(crossProd1,crossProd2)/(np.sin(bondAngle1)*np.sin(bondAngle2))
+        else:
+            return 0.0
+
+        # Check that returned value does not produce infinities
+        if (sol < -1):
+            return np.arccos(-1)
+        elif (sol > 1):
+            return np.arccos(1)
+        else:
+            return np.arccos(sol)
+
+    def calcCOM(self):
+        '''
+            Calculate center of mass position.
+
+            INPUT
+
+            OUTPUT
+                Returns (float) list [x,y,z] of position.
+        '''
+
+        COMpos = [0.0,0.0,0.0]
+        totalMass = 0.0
+
+        for atom in self.atomList:
+            COMpos[0] += atom.mass*atom.xPos
+            COMpos[1] += atom.mass*atom.yPos
+            COMpos[2] += atom.mass*atom.zPos
+            totalMass += atom.mass
+
+        COMpos[0] /= totalMass
+        COMpos[1] /= totalMass
+        COMpos[2] /= totalMass
+
+        return COMpos
+
+    def translate(self,dx,dy,dz):
+        '''
+            Translate the position of the molecule to [x+dx,y+dy,z+dz]. Translates all constituent atoms.
+
+            INPUT
+                dx: (float) Translation in x
+                dy: (float) Translation in y
+                dz: (float) Translation in z
+
+            OUTPUT
+                No output. Updates atom positions.
+        '''
+
+        for atom in self.atomList:
+            atom.translate(dx,dy,dz)
+
+    def inertia(self):
+        '''
+            Calculate principal moments of interia
+
+            INPUT
+
+            OUTPUT
+                Returns list (float) of the PMOI [Ia,Ib,Ic]
+        '''
+
+        # Initialize inertia matrix
+        iMat = np.ones((3,3))
+
+        # Calculate elements
+        for atom in self.atomList:
+            iMat[0,0] += atom.mass*(atom.yPos**2 + atom.zPos**2)
+            iMat[1,1] += atom.mass*(atom.xPos**2 + atom.zPos**2)
+            iMat[2,2] += atom.mass*(atom.xPos**2 + atom.yPos**2)
+
+            iMat[0,1] += atom.mass*atom.xPos*atom.yPos
+            iMat[0,2] += atom.mass*atom.xPos*atom.zPos
+            iMat[1,2] += atom.mass*atom.yPos*atom.zPos
+
+        # Apply symmetry
+        iMat[1,0] += iMat[0,1]
+        iMat[2,0] += iMat[0,2]
+        iMat[2,1] += iMat[1,2]
+
+        # Calculate eigenvalues of iMat & sort
+        eigs = np.linalg.eigvals(iMat)
+        eigs = np.sort(eigs)
+
+        return [eigs[0],eigs[1],eigs[2]]
 
 # Main
 if (__name__ == '__main__'):
@@ -175,15 +292,67 @@ if (__name__ == '__main__'):
         for index2 in range(index,len(molClass.atomList)):
             # Filter out cases where the atom indices are the same
             if (index != index2):
-                print str(index) + '\t' + str(index2) + '\t' + str(molClass.bondLength(atom,molClass.atomList[index2]))
+                ai = atom
+                aj = molClass.atomList[index2]
+                print str(index) + '\t' + str(index2) + '\t' + str(molClass.bondLength(ai,aj))
 
     # Print out bond angles
     print "Index1\tIndex2\tIndex3\tBondAngle"
     for index,atom in enumerate(molClass.atomList):
-        for index2 in range(len(molClass.atomList)):
-            for index3 in range(len(molClass.atomList)):
+        for index2 in range(index):
+            for index3 in range(index2):
+                ai = atom
+                aj = molClass.atomList[index2]
+                ak = molClass.atomList[index3]
+
                 # Filter out cases where the atom indices are the same
                 if ((index != index2) and (index2 != index3) and (index != index3)):
                     # Only calculate bonded atoms
-                    if ((molClass.bondLength(atom,molClass.atomList[index2]) < 4.0) and (molClass.bondLength(atom,molClass.atomList[index3]) < 4.0)):
-                        print str(index2) + '\t' + str(index) + '\t' + str(index3) + '\t' + str(molClass.bondAngle(molClass.atomList[index2],atom,molClass.atomList[index3]))
+                    if ((molClass.bondLength(ai,aj) < 4.0) and (molClass.bondLength(aj,ak) < 4.0)):
+                        print str(index) + '\t' + str(index2) + '\t' + str(index3) + '\t' + str(np.degrees(molClass.bondAngle(ai,aj,ak)))
+
+    # Print OOP angles
+    print "Index1\tIndex2\tIndex3\tIndex4\tOOPAngle"
+    for index,atom in enumerate(molClass.atomList):
+        for index2 in range(len(molClass.atomList)):
+            for index3 in range(len(molClass.atomList)):
+                for index4 in range(index3):
+                    ai = atom
+                    aj = molClass.atomList[index3]
+                    ak = molClass.atomList[index2]
+                    al = molClass.atomList[index4]
+
+                    # Filter out cases where the atom indices are the same and use bonded set of atoms only
+                    if ((index != index2) and (index != index3) and (index != index4) and (index2 != index3) and (index2 != index4) and (molClass.bondLength(ai,ak) < 4.0) and (molClass.bondLength(ak,aj) < 4.0) and (molClass.bondLength(ak,al) < 4.0)):
+                        print str(index) + '\t' + str(index3) + '\t' + str(index2) + '\t' + str(index4) + '\t' + str(np.degrees(molClass.OOPAngle(ai,aj,ak,al)))
+
+    # Print Torsion angles
+    print "Index1\tIndex2\tIndex3\tIndex4\tTorsionAngle"
+    for index,atom in enumerate(molClass.atomList):
+        for index2 in range(index):
+            for index3 in range(index2):
+                for index4 in range(index3):
+                    ai = atom
+                    aj = molClass.atomList[index2]
+                    ak = molClass.atomList[index3]
+                    al = molClass.atomList[index4]
+
+                    # Filter out cases where the atom indices are the same and use bonded set of atoms only
+                    if ((molClass.bondLength(ai,aj) < 4.0) and (molClass.bondLength(aj,ak) < 4.0) and (molClass.bondLength(ak,al) < 4.0)):
+                        print str(index) + '\t' + str(index2) + '\t' + str(index3) + '\t' + str(index4) + '\t' + str(np.degrees(molClass.torsionAngle(ai,aj,ak,al)))
+
+    # Print center of mass and translate molecule
+    COM = molClass.calcCOM()
+    molClass.translate(-COM[0],-COM[1],-COM[2])
+    print "Center of mass: " + str(COM[0]) + '\t' + str(COM[1]) + '\t' + str(COM[2])
+
+    # Calculate principal moments of inertia
+    PMOI = molClass.inertia()
+    # Note that the values are slightly different! Which one is correct?!
+    print PMOI
+
+    # Calculate rotational constants
+    A = (sc.h/sc.c)*(1.0/(8.0*np.pi*np.pi)/(0.5291772e-10*0.5291772e-10*1.6605389e-27))
+    B = (sc.h/sc.c)*(1.0/(8.0*np.pi*np.pi)/(0.5291772e-10*0.5291772e-10*1.6605389e-27))
+    C = (sc.h/sc.c)*(1.0/(8.0*np.pi*np.pi)/(0.5291772e-10*0.5291772e-10*1.6605389e-27))
+    print "Rotational Constants: A=" + str(A/PMOI[0]/100) + '\tB=' + str(B/PMOI[1]/100) + '\tC=' + str(C/PMOI[2]/100)
